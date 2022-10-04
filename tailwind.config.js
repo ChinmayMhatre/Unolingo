9638252fd63d4d91ca01a46e82a32c1c85244365
chinmay <chinmaymhatre12@gmail.com>
2022-10-05 00:27:28 +0530

    complete UI

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                "duo-lingo": "#58cc02",
+            },
+        },
+        plugins: [],
+    },
+};
